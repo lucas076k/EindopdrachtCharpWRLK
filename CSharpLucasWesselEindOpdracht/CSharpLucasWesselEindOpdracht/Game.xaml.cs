f1074ea68b7c6ad18669a3fd6f22a2ce2b18b9bc
WesselRo <123960059+WesselRo@users.noreply.github.com>
2023-10-18 10:42:27 +0200

    Added lose and draw feature

--- CSharpLucasWesselEindOpdracht/CSharpLucasWesselEindOpdracht/Game.xaml.cs
+++ CSharpLucasWesselEindOpdracht/CSharpLucasWesselEindOpdracht/Game.xaml.cs
@@ -19,9 +19,112 @@ namespace CSharpLucasWesselEindOpdracht
     /// </summary>
     public partial class Game : Window
     {
+        
+        private char PlayerChar;
+        private char OppenentChar;
+
         public Game()
         {
             InitializeComponent();
+        }
+        
+        private bool Winner()
+        {
+            if(button1.Content == button2.Content && button1.Content == button3.Content && !button1.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+                
+                return true;
+            }
+
+            else if(button4.Content == button5.Content && button4.Content == button6.Content && !button4.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if(button7.Content == button8.Content && button7.Content == button9.Content && !button7.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if (button1.Content == button4.Content && button1.Content == button7.Content && !button1.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if (button2.Content == button5.Content && button2.Content == button8.Content && !button2.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if (button3.Content == button6.Content && button3.Content == button9.Content && !button3.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if (button1.Content == button5.Content && button1.Content == button9.Content && !button1.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            else if (button3.Content == button5.Content && button3.Content == button7.Content && !button3.Content.Equals(""))
+            {
+                if (button1.Content.Equals(PlayerChar)) WinGame();
+
+                else LoseGame();
+
+                return true;
+            }
+
+            // draw
+            else if (!button1.Content.Equals("") && !button2.Content.Equals("") && !button3.Content.Equals("") && !button4.Content.Equals("") && 
+                !button5.Content.Equals("") && !button6.Content.Equals("") && !button7.Content.Equals("") && !button8.Content.Equals("") && !button9.Content.Equals(""))
+            {
+                DrawGame();
+            }
+
+            return false;
+        }
+
+        private void WinGame()
+        {
+            label1.Content = "You won!";
+        }
+
+        private void LoseGame()
+        {
+            label1.Content = "You lost :(";
+        }
+
+        private void DrawGame()
+        {
+            label1.Content = "Draw!";
         }
 
     }
