58e23df7238d4f532966aa30e6588027cadcd563
WesselRo <123960059+WesselRo@users.noreply.github.com>
2023-10-18 11:33:15 +0200

    Game logic implemented

--- CSharpLucasWesselEindOpdracht/CSharpLucasWesselEindOpdracht/Game.xaml.cs
+++ CSharpLucasWesselEindOpdracht/CSharpLucasWesselEindOpdracht/Game.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.ComponentModel;
 
 namespace CSharpLucasWesselEindOpdracht
 {
@@ -20,19 +22,59 @@ namespace CSharpLucasWesselEindOpdracht
     public partial class Game : Window
     {
         
-        private char PlayerChar;
-        private char OppenentChar;
+        private char playerChar;
+        private char oppenentChar;
+        private Socket socket;
+        private BackgroundWorker worker = new BackgroundWorker();
+        private TcpListener server;
+        private TcpClient client;
 
-        public Game()
+        public Game(bool isHost, string ip = null)
         {
             InitializeComponent();
+            worker.DoWork += Worker_DoWork;
+
+            if (isHost)
+            {
+                playerChar = 'X'; 
+                oppenentChar = 'O';
+                server = new TcpListener(System.Net.IPAddress.Any, 123);
+                server.Start();
+                socket = server.AcceptSocket();
+            }
+            else
+            {
+                playerChar = 'O';
+                oppenentChar = 'X';
+
+                try
+                {
+                    client = new TcpClient(ip, 123);
+                    socket = client.Client;
+                    worker.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
         }
-        
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            if (Winner()) return;
+            DisableButtons();
+            label1.Content = "Opponent's turn";
+            GetOppenentMove();
+            if (!Winner()) EnableEmptyButtons(); label1.Content = "Your turn";
+        }
+
         private bool Winner()
         {
             if(button1.Content == button2.Content && button1.Content == button3.Content && !button1.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
                 
@@ -41,7 +83,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if(button4.Content == button5.Content && button4.Content == button6.Content && !button4.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -50,7 +92,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if(button7.Content == button8.Content && button7.Content == button9.Content && !button7.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -59,7 +101,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if (button1.Content == button4.Content && button1.Content == button7.Content && !button1.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -68,7 +110,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if (button2.Content == button5.Content && button2.Content == button8.Content && !button2.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -77,7 +119,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if (button3.Content == button6.Content && button3.Content == button9.Content && !button3.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -86,7 +128,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if (button1.Content == button5.Content && button1.Content == button9.Content && !button1.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -95,7 +137,7 @@ namespace CSharpLucasWesselEindOpdracht
 
             else if (button3.Content == button5.Content && button3.Content == button7.Content && !button3.Content.Equals(""))
             {
-                if (button1.Content.Equals(PlayerChar)) WinGame();
+                if (button1.Content.Equals(playerChar)) WinGame();
 
                 else LoseGame();
 
@@ -107,6 +149,8 @@ namespace CSharpLucasWesselEindOpdracht
                 !button5.Content.Equals("") && !button6.Content.Equals("") && !button7.Content.Equals("") && !button8.Content.Equals("") && !button9.Content.Equals(""))
             {
                 DrawGame();
+
+                return true;
             }
 
             return false;
@@ -127,5 +171,78 @@ namespace CSharpLucasWesselEindOpdracht
             label1.Content = "Draw!";
         }
 
+        private void DisableButtons()
+        {
+            button1.IsEnabled = false;
+            button2.IsEnabled = false;
+            button3.IsEnabled = false;
+            button4.IsEnabled = false;
+            button5.IsEnabled = false;
+            button6.IsEnabled = false;
+            button7.IsEnabled = false;
+            button8.IsEnabled = false;
+            button9.IsEnabled = false;
+        }
+
+        private void EnableEmptyButtons()
+        {
+            if (button1.Equals("")) button1.IsEnabled = true;
+            if (button2.Equals("")) button2.IsEnabled = true;
+            if (button3.Equals("")) button3.IsEnabled = true;
+            if (button4.Equals("")) button4.IsEnabled = true;
+            if (button5.Equals("")) button5.IsEnabled = true;
+            if (button6.Equals("")) button6.IsEnabled = true;
+            if (button7.Equals("")) button7.IsEnabled = true;
+            if (button8.Equals("")) button8.IsEnabled = true;
+            if (button9.Equals("")) button9.IsEnabled = true;
+        }
+
+        private void GetOppenentMove()
+        {
+            byte[] buttonByte = new byte[1];
+            socket.Receive(buttonByte);
+
+            if (buttonByte[0] == 1) button1.Content = oppenentChar;
+            if (buttonByte[0] == 2) button2.Content = oppenentChar;
+            if (buttonByte[0] == 3) button3.Content = oppenentChar;
+            if (buttonByte[0] == 4) button4.Content = oppenentChar;
+            if (buttonByte[0] == 5) button5.Content = oppenentChar;
+            if (buttonByte[0] == 6) button6.Content = oppenentChar;
+            if (buttonByte[0] == 7) button7.Content = oppenentChar;
+            if (buttonByte[0] == 8) button8.Content = oppenentChar;
+            if (buttonByte[0] == 9) button9.Content = oppenentChar;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 1 }, button1);}
+
+        private void button2_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 2 }, button2);}
+
+        private void button3_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 3 }, button3);}
+
+        private void button4_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 4 }, button4);}
+
+        private void button5_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 5 }, button5);}
+
+        private void button6_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 6 }, button6);}
+
+        private void button7_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 7 }, button7);}
+
+        private void button8_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 8 }, button8);}
+
+        private void button9_Click(object sender, RoutedEventArgs e) {buttonClick(new Byte[] { 9 }, button9);}
+
+        private void buttonClick(byte[] buttonNumber, Button button)
+        {
+            socket.Send(buttonNumber);
+            button.Content = playerChar;
+            worker.RunWorkerAsync();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            worker.WorkerSupportsCancellation = true;
+            worker.CancelAsync();
+            if (server != null) server.Stop();
+        }
     }
 }
