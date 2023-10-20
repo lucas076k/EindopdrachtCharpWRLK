25c83efaa11a937c982193f8bf332489615b3bb4
WesselRo <123960059+WesselRo@users.noreply.github.com>
2023-10-20 09:40:32 +0200

    async added

--- EindopdrachtChatServ/Client/MainWindow.xaml.cs
+++ EindopdrachtChatServ/Client/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -34,18 +35,20 @@ namespace Client
             string fileContent = Encoding.ASCII.GetString(buffer, 0, bytesRead);
             Dispatcher.Invoke(() => { chatListBox.Items.Add(fileContent); });
             
+            Dispatcher.Invoke(() => { chatListBox.Items.Add("You joined the chat"); });
+            SendToServer(clientUsername + " has joined the chat");
+            
         }
 
-        private void ReceiveMessages()
+        private async void ReceiveMessages()
         {
             while (true)
             {
                 byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
                 string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
-                Dispatcher.Invoke(() => { chatListBox.Items.Add(message); });
                 
+                Dispatcher.Invoke(() => { chatListBox.Items.Add(message); });
             }
         }
 
@@ -56,10 +59,8 @@ namespace Client
             bool whiteLine = string.IsNullOrWhiteSpace(message);
             if (!whiteLine)
             {
-                // Adds message to ListBox
                 Dispatcher.Invoke(() => { chatListBox.Items.Add("You: " + message); });
                 
-                // Sends message to other clients
                 SendToServer(clientUsername + ": " + message);
             }
             
