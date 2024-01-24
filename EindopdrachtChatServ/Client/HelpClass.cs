cd60d3628f4514e8a4ca21716c24fad0d3723adf
WesselRo <123960059+WesselRo@users.noreply.github.com>
2024-01-24 12:49:37 +0100

    iushdfas

---
+++ EindopdrachtChatServ/Client/HelpClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class HelpClass
+    {
+        public bool ReturnTrueIfNotEmpty(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
